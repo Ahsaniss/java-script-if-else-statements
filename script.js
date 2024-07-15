fdcd4e95375001aa500afdc2903bcb113b9a549b
Ahsaniss <ahsanraza85858585@gmail.com>
2024-07-15 03:02:00 -0700

    map code

--- script.js
+++ script.js
@@ -172,3 +172,12 @@ function calculateElectricityBill(units) {
 }
 
 console.log(calculateElectricityBill(200)); // Output: 135 (assuming 200 units consumed)
+let john = { name: "John" };
+
+// for every user, let's store their visits count
+let visitsCountMap = new Map();
+
+// john is the key for the map
+visitsCountMap.set(john, 123);
+
+alert( visitsCountMap.get(john) ); // 123
